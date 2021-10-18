a8d8c2bedd66f493d7095b7d2f202c9c1ce9030b
Anderson Alencar <anderson23alencar@gmail.com>
2021-10-18 19:13:45 -0300

    Route com passagem de props

--- src/components/Users.js
+++ src/components/Users.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 
 class Users extends Component {
   render() {
+    const { greetingsMessage } = this.props;
+    const { id } = this.props.match.params;
+    console.log(id);
     return (
       <div>
         <h2>Users</h2>
-        <p> My awesome Users component </p>
+        <p> { greetingsMessage }, My awesome {id} Users component </p>
       </div>
     );
   }
